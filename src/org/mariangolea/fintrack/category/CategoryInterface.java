5d40e432263b24288400a10de2bf2e5fe5bac4d7
Marian <mariangolea@gmail.com>
2021-11-28 11:39:31 +0200

    Extended CategoriesInterface

--- src/org/mariangolea/fintrack/category/CategoryInterface.java
+++ src/org/mariangolea/fintrack/category/CategoryInterface.java
@@ -1,12 +1,18 @@
 package org.mariangolea.fintrack.category;
 
+import java.util.Collection;
+
 public interface CategoryInterface {
 
 	public String getName();
 
 	public void setName(String name);
 
-	public Long getParent();
-
-	public void setParent(Long parent);
+	public void setParentCategory(CategoryInterface parent);
+	
+	public CategoryInterface getParent();
+	
+	public Collection<CategoryInterface> getChildren();
+	
+	public void addChildren(CategoryInterface... children);
 }
